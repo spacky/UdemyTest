f3768667ddea2695920a213c05c45b58a61d5785
spacky <spackyggo@gmail.com>
2020-10-19 20:00:32 +0300

    first save

---
+++ js/script.js
@@ -0,0 +1,52 @@
+ "use strict";
+ 
+/*let number = 4.6;
+
+console.log(4/0);
+console.log('string' * 9);
+
+const persone = '5';
+console.log(number + persone);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+}
+
+//console.log(obj.age)
+console.log(obj.name);
+
+let arr = ['John', 25, false];
+console.log(arr[0]);*/
+
+//alert('Hello');
+//const result = confirm('Are you here?');
+//console.log(result);
+
+//const answer = prompt('Вам есть 18?', 'Если нет то вас выкенет с сайта буууум');
+//console.log(answer);
+
+/*const answers = [];
+
+answers[0] = prompt('Как ваше имя?');
+answers[1] = +prompt('Сколько вам лет?');
+answers[2] = prompt('Ваше отчество?');*/
+
+/*const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5/${category}/`);
+
+const user = 'Ivan';
+alert(`Hi, ${user}!`);*/
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
